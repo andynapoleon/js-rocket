87c71b2247ea7a4bca1071678c9967323968de10
andynapoleon <anhquoctran006@gmail.com>
2021-06-18 11:15:23 -0300

    changed something

--- js/rocket.js
+++ js/rocket.js
@@ -36,7 +36,7 @@ var changeState = function (state) {
 			var randomNumber = Math.round(Math.random()*10);
 			console.log('randomNumber:',randomNumber)
 			//success
-			if (randomNumber > 2) {
+			if (randomNumber > 5) {
 				changeState(4);
 			} else {
 				changeState(5); //ohno!
