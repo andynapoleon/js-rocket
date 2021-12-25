18f8d20a5caaed018a214315e1e4774f526d87f6
andynapoleon <85133277+andynapoleon@users.noreply.github.com>
2021-12-25 13:37:37 -0700

    add files that were not uploaded before

---
+++ explanations.js
@@ -0,0 +1,69 @@
+// Explanations
+
+// Variables
+var favColor = "black";
+var myFavColors = ['black','white','blue'];
+var numOfFavColors = 3;
+var hasGotFavColors = false;
+var andyObject = {
+	firstName: "Andy",
+	lastname: "Armstrong",
+	myFavColors: ['black','white','blue'],
+	yearsAlive: 103,
+	isMale: true
+};
+
+// Functions
+
+var fullname = function (firstname, secondname) {
+	return firstname + ' ' +secondname;
+}
+
+var area = function (width,height) {
+	return width*height;
+}
+
+var whatismyfavcolor = function  () {
+	
+	return false;
+
+	return true;
+
+	return 'blue';
+}
+
+var doSomething = function  () {
+	console.log('do Something!!!')
+}
+
+// If Statements
+
+if(true) {
+	alert('male');
+}
+
+if(1 == 2) {
+	alert('male');
+}
+
+var number1 = 10;
+var number2 = 100;
+
+if(number1 > number2) {
+	alert('true!');
+} else {
+	alert('false!!');
+}
+
+var name1 = 'cac';
+var name2 = 'Kate';
+
+if(name1 == 'Kate' && name2 = 'Kate') {
+	alert('true 1!');
+} else if (name2 == 'Kate') {
+	alert('true 2!'); 
+} else {
+	alert('false!');
+}
+
+
---
+++ js-script.js
@@ -0,0 +1,51 @@
+
+setTimeout(function() {
+	location.href = 'rocket.html';
+}, 2000)
+
+// this function does this.
+var doCoolStuff = function () {
+	
+	// changing back and forth between class names
+	var currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+}
+
+var sayMyName = function (name) {
+	alert('My name is' + name);
+}
+
+var car = {
+			make: 'VW',
+			type: 'Polo',
+			color: 'blue',
+			isTurnedon: false,
+			numberofWheels: 4,
+			seats: [
+				'seat 1', 
+				'seat 2', 
+				'seat 3', 
+				'seat 4'
+			],
+			turnOn: function() {
+				this.isTurnedon = true;
+			},
+			fly: function() {
+				alert('fly');
+			},
+			switchCar: function(isOn) {
+				console.log('turn car ' + isOn);
+				if (isOn == true) {
+					this.isTurnedon = true;
+				} else {
+					this.isTurnedon = false;
+				}
+			}
+		};
+		
+		console.log('hello there friends!');
